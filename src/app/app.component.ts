4d2024beef933febf361c836a9bc06f3ab5b1fd3
Danuka Malawana <danukaem@gmail.com>
2021-01-20 19:12:58 +0530

    ip address service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IpServiceService} from './ip-service.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FinalYearProject';
+export class AppComponent implements OnInit {
+  ipAddress: string;
+
+  constructor(private http: HttpClient, private ip: IpServiceService) {
+  }
+
+  ngOnInit() {
+    this.ip.getIp().subscribe((response: any) => {
+      this.ip.ipAddress = response.ip;
+      this.ipAddress = this.ip.ipAddress;
+
+    });
+  }
+
+
 }
